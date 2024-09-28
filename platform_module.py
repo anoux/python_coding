72fc9a1ca26bd08ef9b6cb5dda73c3054ab1f341
Anoux Jadur <ajadur96@gmail.com>
2024-09-28 15:13:22 +0200

    some functions from platform module

---
+++ platform_module.py
@@ -0,0 +1,12 @@
+# functions from platform module
+
+from platform import platform, machine, processor, system
+
+print(platform(aliased = False, terse = False))
+
+print(machine())
+ 
+print(processor())
+
+print(system())
+ 
