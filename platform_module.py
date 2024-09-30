dd036612825afd16b44f5cd87294a85b07c72523
Anoux Jadur <ajadur96@gmail.com>
2024-09-30 15:16:41 +0200

    modules poc - my first module

--- platform_module.py
+++ platform_module.py
@@ -1,6 +1,6 @@
 # functions from platform module
 
-from platform import platform, machine, processor, system
+from platform import platform, machine, processor, system, version
 
 print(platform(aliased = False, terse = False))
 
@@ -9,4 +9,5 @@ print(machine())
 print(processor())
 
 print(system())
- 
+
+print(version())
